0d754867cc22719338dd1e09b9ecfb8a02f0a4bf
mhadziqrazin <mhadziq0403@gmail.com>
2023-06-29 22:55:27 +0700

    feat: test auth button login and sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+'use client'
+
+import { signIn, signOut } from 'next-auth/react'
+
 export default function Home() {
   return (
     <main>
-      Halo
+      <button
+        onClick={() => signIn('google')}
+        className="bg-black text-white"
+      >
+        login with google
+      </button>
+      <button
+        onClick={() => signOut()}
+        className='bg-red-500 text-white'
+      >
+        sign out
+      </button>
     </main>
   )
 }
