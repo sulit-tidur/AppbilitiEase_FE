8dbc3b993e40c15ded6cc49a5a23718c7588a7a0
mhadziqrazin <mhadziq0403@gmail.com>
2023-07-05 23:02:31 +0700

    feat: add type to facilities placeholder

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,26 +1,23 @@
-import { Developer } from "./types"
+import { Developer, Facility } from "./types"
 
-export const facilities = [
+export const facilities: Facility[] = [
   {
-    id: 1,
     title: 'Fakultas Ilmu Komputer UI',
-    desc: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a BC, making it over 2000 years old....',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a BC, making it over 2000 years old....',
     location: 'Depok, Indonesia',
     src: '/images/pacil.svg',
     href: '/'
   },
   {
-    id: 2,
     title: 'Fakultas Ilmu Komputer UI',
-    desc: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a BC, making it over 2000 years old....',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a BC, making it over 2000 years old....',
     location: 'Depok, Indonesia',
     src: '/images/pacil.svg',
     href: '/'
   },
   {
-    id: 3,
     title: 'Fakultas Ilmu Komputer UI',
-    desc: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a BC, making it over 2000 years old....',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a BC, making it over 2000 years old....',
     location: 'Depok, Indonesia',
     src: '/images/pacil.svg',
     href: '/'
