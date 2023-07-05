6d6976e4e44d99723132571c3e4b8d4b07712e54
mhadziqrazin <mhadziq0403@gmail.com>
2023-07-05 23:01:03 +0700

    feat: developers data

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
+import { Developer } from "./types"
+
 export const facilities = [
   {
     id: 1,
@@ -23,4 +25,31 @@ export const facilities = [
     src: '/images/pacil.svg',
     href: '/'
   }
+]
+
+export const developer: Developer[] = [
+  {
+    name: 'Muhammad Hadziq Razin',
+    education: 'Mahasiswa Universitas Indonesia',
+    description: 'Prodi Ilmu Komputer angkatan 2021',
+    role: 'Front-end Developer',
+    instagram: 'https://instagram.com/hadziqrazin',
+    linkedin: 'https://www.linkedin.com/in/mhadziqrazin'
+  },
+  {
+    name: 'Jihan Syafa Kamila',
+    education: 'Mahasiswa Universitas Indonesia',
+    description: 'Prodi Sistem Informasi angkatan 2021',
+    role: 'UI/UX Designer',
+    instagram: 'https://www.instagram.com/jihansyafaa',
+    linkedin: 'https://www.linkedin.com/in/jihan-syafa-kamila'
+  },
+  {
+    name: 'Muhammad Hadziq Razin',
+    education: 'Mahasiswa Universitas Indonesia',
+    description: 'Prodi Ilmu Komputer angkatan 2021',
+    role: 'Back-end Developer',
+    instagram: 'https://www.instagram.com/owwlvest',
+    linkedin: 'https://www.linkedin.com/in/jason-irvine-mahendra-putra'
+  },
 ]
