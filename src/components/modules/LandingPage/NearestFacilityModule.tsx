27d9129c79e3706bf656340c448e55ab3a5c4488
mhadziqrazin <mhadziq0403@gmail.com>
2023-07-05 17:07:51 +0700

    style: animation for facility card

--- src/components/modules/LandingPage/NearestFacilityModule.tsx
+++ src/components/modules/LandingPage/NearestFacilityModule.tsx
@@ -1,8 +1,19 @@
+'use client'
+
 import { poppins } from "@/utils/fonts"
 import { facilities } from "@/utils/constants"
 import FacilityCard from "./FacilityCard"
+import { useRef } from "react"
+import { useElementOnScreen } from "@/components/hooks/useElementOnScreen"
 
 const NearestFacilityModule = () => {
+  const first = useRef(null)
+  const firstOnScreen = useElementOnScreen({ ref: first })
+  const second = useRef(null)
+  const secondOnScreen = useElementOnScreen({ ref: second })
+  const third = useRef(null)
+  const thirdOnScreen = useElementOnScreen({ ref: third })
+
   return (
     <section className={`${poppins.className} bg-[url(/images/bg/bg-fasilitas.svg)] bg-center bg-cover bg-no-repeat h-fit flex`}>
       <div className="container mx-auto p-4 md:max-w-[calc(100%-100px)] mb-20 mt-[430px] xxs:mt-[330px] md:mt-[220px] xl:mt-[170px]">
@@ -19,9 +30,24 @@ const NearestFacilityModule = () => {
 
           {/* Facilities */}
           <div className="flex flex-col md:flex-row gap-6">
-            {facilities.slice(0, 3).map((facility) => (
-              <FacilityCard key={facility.id} facility={facility} />
-            ))}
+            <div
+              ref={first}
+              className={`transition duration-1000 ${firstOnScreen ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'}`}
+            >
+              <FacilityCard facility={facilities[0]} />
+            </div>
+            <div
+              ref={second}
+              className={`transition delay-100 duration-1000 ${secondOnScreen ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'}`}
+            >
+              <FacilityCard facility={facilities[1]} />
+            </div>
+            <div
+              ref={third}
+              className={`transition delay-200 duration-1000 ${thirdOnScreen ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'}`}
+            >
+              <FacilityCard facility={facilities[2]} />
+            </div>
           </div>
         </div>
       </div>
