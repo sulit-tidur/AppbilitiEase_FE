f740b020712bdc8e7208ad81121d03cae033aa92
mhadziqrazin <mhadziq0403@gmail.com>
2023-07-05 20:49:41 +0700

    style: animation for education and news section

--- src/components/modules/LandingPage/EducationModule.tsx
+++ src/components/modules/LandingPage/EducationModule.tsx
@@ -1,9 +1,23 @@
+'use client'
+
+import { useElementOnScreen } from "@/components/hooks/useElementOnScreen"
 import { nunito, poppins, roboto } from "@/utils/fonts"
 import Image from "next/image"
 import Link from "next/link"
+import { useRef } from "react"
 import { IoIosArrowDroprightCircle } from "react-icons/io"
 
 const EducationModule = () => {
+
+  const orangFirstRef = useRef(null)
+  const orangFirstOnScreen = useElementOnScreen({ ref: orangFirstRef })
+  const orangSecondRef = useRef(null)
+  const orangSecondOnScreen = useElementOnScreen({ ref: orangSecondRef })
+  const edukasiRef = useRef(null)
+  const edukasiOnScreen = useElementOnScreen({ ref: edukasiRef })
+  const beritaRef = useRef(null)
+  const beritaOnScreen = useElementOnScreen({ ref: beritaRef })
+
   return (
     <section className="h-fit bg-cream">
       <div className="container mx-auto px-4 py-14 md:max-w-[calc(100%-100px)]">
@@ -11,8 +25,11 @@ const EducationModule = () => {
           {/* Education & News Illustration */}
           <Image
             src={'/images/group-gambar-section-3.svg'}
-            width={614} height={635} alt="Edukasi dan Berita"
-            className="block sm:hidden lg:block w-[300px] lg:w-[450px] xl:w-[614px]"
+            width={614} height={635} alt="Edukasi dan Berita" ref={orangFirstRef}
+            className={`
+              block sm:hidden lg:block w-[300px] lg:w-[450px] xl:w-[614px] transition duration-1000
+              ${orangFirstOnScreen ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-10'}
+            `}
           />
           <div className="flex flex-col gap-14">
             {/* Title */}
@@ -25,13 +42,22 @@ const EducationModule = () => {
             <div className="flex gap-4">
               <Image
                 src={'/images/group-gambar-section-3.svg'}
-                width={614} height={635} alt="Edukasi dan Berita"
-                className="hidden sm:block lg:hidden w-[300px]"
+                width={614} height={635} alt="Edukasi dan Berita" ref={orangSecondRef}
+                className={`
+                  hidden sm:block lg:hidden w-[300px] transition duration-1000
+                  ${orangSecondOnScreen ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-10'}
+                `}
               />
 
               <div className={`${roboto.className} flex flex-col gap-6`}>
-                {/* Education & News Info */}
-                <div className="flex gap-4 items-center">
+                {/* Education Info */}
+                <div
+                  ref={edukasiRef}
+                  className={`
+                    flex gap-4 items-center transition duration-1000
+                    ${edukasiOnScreen ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'}
+                  `}
+                >
                   <Image
                     src={'/images/edukasi-icon-2.svg'}
                     width={101} height={101} alt="Edukasi"
@@ -46,7 +72,15 @@ const EducationModule = () => {
                     </p>
                   </div>
                 </div>
-                <div className="flex gap-4 items-center">
+
+                {/* News Info */}
+                <div
+                  ref={beritaRef}
+                  className={`
+                    flex gap-4 items-center transition duration-1000
+                    ${beritaOnScreen ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'}
+                  `}
+                >
                   <Image
                     src={'/images/berita-icon.svg'}
                     width={101} height={101} alt="Edukasi"
