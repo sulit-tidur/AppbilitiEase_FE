c887e8e3ddc415651d4b57bcc280ad99d1916118
mhadziqrazin <mhadziq0403@gmail.com>
2023-07-06 19:32:46 +0700

    style: animation for login page image

--- src/components/modules/LoginPage/LoginModule.tsx
+++ src/components/modules/LoginPage/LoginModule.tsx
@@ -1,17 +1,26 @@
 'use client'
 
+import { useElementOnScreen } from "@/components/hooks/useElementOnScreen"
 import { poppins } from "@/utils/fonts"
 import Image from "next/image"
+import { useRef } from "react"
 
 const LoginModule = () => {
+
+  const ref = useRef(null)
+  const isOnScreen = useElementOnScreen({ ref })
+
   return (
     <section className={`${poppins.className} bg-[url(/images/bg/bg-login.svg)] bg-center bg-cover bg-no-repeat h-fit min-h-screen flex`}>
       <div className="container justify-center mx-auto px-4 md:max-w-[calc(100%-100px)] py-16 flex mt-[50px] md:mt-[88px]">
         <div className="flex items-center justify-center flex-col-reverse md:flex-row gap-x-14 lg:gap-x-20 xl:gap-x-40 gap-y-14">
           <Image
             src={'/images/group-login.svg'}
-            width={733} height={636} alt="Login"
-            className="w-[300px] lg:w-[450px] xl:w-[600px] 2xl:w-[733px]"
+            width={733} height={636} alt="Login" ref={ref}
+            className={`
+              w-[300px] lg:w-[450px] xl:w-[600px] 2xl:w-[733px] transition duration-1000
+              ${isOnScreen ? 'opacity-100 translate-x-0' : 'opacity-0 motion-safe:-translate-x-10'}
+            `}
           />
           <div className="bg-white px-4 py-10 lg:px-10 lg:py-16 rounded-xl shadow-md flex flex-col gap-8 xl:gap-14">
             <h1 className="font-semibold text-3xl text-dark text-center">
