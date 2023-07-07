b92dc44261051491b840f65c6bc190f987c723a3
mhadziqrazin <mhadziq0403@gmail.com>
2023-07-07 14:14:40 +0700

    style: search bar animation

--- src/components/modules/EducationNewsPage/SearchSection.tsx
+++ src/components/modules/EducationNewsPage/SearchSection.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { FormEvent, useState } from "react"
+import { FormEvent, useRef, useState } from "react"
 import { PiMagnifyingGlassBold } from "react-icons/pi"
 
 const SearchSection = () => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState<string>('')
+  const ref = useRef<HTMLInputElement>(null)
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -12,32 +13,39 @@ const SearchSection = () => {
   }
 
   return (
-    <div className="flex py-14 sm:px-14">
+    <section className="flex pt-8 md:pt-14 sm:px-14">
       <form onSubmit={handleSubmit} className="w-full">
-        <div className="
-            border-2 border-dark rounded-full p-[4px] h-[50px] xs:h-[68px] flex items-center place-content-between
-            transition hover:shadow-lg
+        <div onClick={() => ref.current?.focus()}
+          className="
+            border-2 border-dark rounded-full py-[4px] h-[50px] xs:h-[68px] flex items-center justify-center
+            transition hover:shadow-lg relative overflow-hidden hover:cursor-pointer
           "
         >
-          <PiMagnifyingGlassBold size={15} className="mx-4 min-w-[15px]" />
+          <PiMagnifyingGlassBold size={15} className="mx-4 xs:mx-0 xs:absolute xs:left-[20px]" />
           <input
-            type="text" placeholder="Cari edukasi atau berita.."
+            type="text" placeholder="Cari edukasi atau berita.." ref={ref}
             value={value} onChange={(e) => setValue(e.target.value)}
             className={`
-              mr-4 ${value === '' ? 'w-full sm:w-[222px]' : 'w-full'} focus:w-full cursor-default focus:cursor-text outline-none
-              motion-safe:transition-all motion-safe:duration-1000 ease-out
+            xs:focus:w-[calc(100%-102px)] cursor-pointer focus:cursor-text outline-none motion-reduce:w-[calc(100%-102px)] motion-reduce:pr-[107px] motion-reduce:md:pr-[157px]
+            ${value === '' ? 'w-full xs:motion-safe:w-[195px] pr-0' : 'w-[calc(100%-102px)] pr-[107px] md:pr-[157px]'}
+            motion-safe:transition-all motion-safe:duration-1000 ease-out peer
             `}
           />
           <button
             type="submit"
-            className="self-end hidden xs:block bg-purple p-4 px-14 md:px-20 rounded-full">
-            <p className="text-white font-medium">
+            className={`
+              self-end hidden xs:block bg-purple rounded-full transition-all delay-300 duration-1000 ease-out
+              absolute right-[4px] ${value == '' ? 'motion-reduce:opacity-0 motion-safe:translate-x-[110%]' : 'motion-reduce:opacity-100 translate-x-0'} peer-focus:translate-x-0
+              motion-reduce:peer-focus:opacity-100
+            `}
+          >
+            <p className="p-4 px-14 md:px-20 text-white font-medium">
               Cari
             </p>
           </button>
         </div>
       </form>
-    </div>
+    </section>
   )
 }
 
