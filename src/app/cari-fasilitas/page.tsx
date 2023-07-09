9f07563aeb9615ea9305f841dc363b11a3ace588
mhadziqrazin <mhadziq0403@gmail.com>
2023-07-09 09:20:49 +0700

    feat: init cari fasilitas page

---
+++ src/app/cari-fasilitas/page.tsx
@@ -0,0 +1,9 @@
+import HeroModule from '@/components/modules/HeroModule'
+
+export default async function FindFacility() {
+  return (
+    <>
+      <HeroModule title='Cari Fasilitas' bg='bg-[url(/images/bg/bg-cari-fasilitas.svg)]' />
+    </>
+  )
+}
