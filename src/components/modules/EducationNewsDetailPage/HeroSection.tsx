16d00cf4f10d0f0eed564787d01712785df66d64
mhadziqrazin <mhadziq0403@gmail.com>
2023-07-10 22:52:17 +0700

    refactor: back button

--- src/components/modules/EducationNewsDetailPage/HeroSection.tsx
+++ src/components/modules/EducationNewsDetailPage/HeroSection.tsx
@@ -1,9 +1,6 @@
-'use client'
-
+import BackButton from '@/components/Buttons/BackButton'
 import { poppins } from '@/utils/fonts'
 import { Article } from '@/utils/types'
-import { useRouter } from 'next/navigation'
-import { IoIosArrowDropleftCircle } from 'react-icons/io'
 
 interface HeroSectionProps {
   article: Article
@@ -12,19 +9,10 @@ interface HeroSectionProps {
 const HeroSection: React.FC<HeroSectionProps> = ({
   article
 }) => {
-  const router = useRouter()
   return (
     <section className={`${poppins.className} bg-[url(/images/bg/bg-edukasi-berita.svg)] relative bg-center bg-cover bg-no-repeat h-fit flex`}>
       <div className='container mx-auto mt-[88px] px-4 md:px-20 pt-[calc((50vh-88px)/2)] pb-[15vh] flex flex-col gap-4'>
-        <button
-          onClick={() => router.back()}
-          className='flex items-center gap-[4px] group w-fit'
-        >
-          <IoIosArrowDropleftCircle />
-          <p className='font-normal group-hover:underline'>
-            Kembali
-          </p>
-        </button>
+        <BackButton />
         <h1 className='font-semibold break-words text-header text-dark'>
           {article.title}
         </h1>
--- src/components/modules/FindFacilityDetailPage/HeroSection.tsx
+++ src/components/modules/FindFacilityDetailPage/HeroSection.tsx
@@ -1,8 +1,7 @@
 'use client'
 
+import BackButton from '@/components/Buttons/BackButton'
 import { poppins } from '@/utils/fonts'
-import { useRouter } from 'next/navigation'
-import { IoIosArrowDropleftCircle } from 'react-icons/io'
 
 interface HeroSectionProps {
   facilityName: string
@@ -11,19 +10,10 @@ interface HeroSectionProps {
 const HeroSection: React.FC<HeroSectionProps> = ({
   facilityName
 }) => {
-  const router = useRouter()
   return (
     <section className={`${poppins.className} bg-[url(/images/bg/bg-cari-fasilitas.svg)] relative bg-center bg-cover bg-no-repeat h-fit flex`}>
       <div className='container mx-auto mt-[88px] px-4 sm:px-12 pt-[calc((50vh-88px)/2)] pb-[15vh] flex flex-col gap-4'>
-        <button
-          onClick={() => router.back()}
-          className='flex items-center gap-[4px] group w-fit'
-        >
-          <IoIosArrowDropleftCircle />
-          <p className='font-normal group-hover:underline'>
-            Kembali
-          </p>
-        </button>
+        <BackButton />
         <h1 className='font-semibold text-header text-dark'>
           {facilityName}
         </h1>
