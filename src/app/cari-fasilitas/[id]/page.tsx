a94c772e70aac45958f97f6ba54b1025f07c5165
mhadziqrazin <mhadziq0403@gmail.com>
2023-07-10 14:09:42 +0700

    feat: 404 page

--- src/app/cari-fasilitas/[id]/page.tsx
+++ src/app/cari-fasilitas/[id]/page.tsx
@@ -1,9 +1,17 @@
 import HeroSection from '@/components/modules/FindFacilityDetailPage/HeroSection'
 import BodySection from '@/components/modules/FindFacilityDetailPage/BodySection'
 import getFacilities from '@/utils/actions/getFacilities'
+import NotFound from '@/app/not-found'
 
 export default async function Fasilitas({ params }: { params: { id: string } }) {
   const facility = await getFacilities({ id: Number(params.id) })
+
+  if (!facility[0]) {
+    return (
+      <NotFound message='Fasilitas' />
+    )
+  }
+
   return (
     <>
       <HeroSection facilityName={facility[0].name} />
